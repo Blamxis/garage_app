d59ec292b98880f9d216cabe47d06fd2291edf7b
Blamxis <gavinetm26@gmail.com>
2023-12-19 02:34:36 +0100

    Ajout du model Role

--- BackEnd/Src/Models/Roles.js
+++ BackEnd/Src/Models/Roles.js
@@ -1,16 +1,17 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../Utils/db.config');
 
-class Role extends Model {}
+class Roles extends Model {}
 
-Role.init ({
-    Nom: {
-        type: DataTypes.STRING(50),
-        allowNull: false
-    }
-}, {
-    sequelize,
-    modelName:'role'
-});
+module.exports = (sequelize) => {
+    Roles.init({
+        Nom: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+        }
+    }, {
+        sequelize,
+        modelName: 'Roles'
+    });
 
-module.exports = Role;
+    return Roles;
+};
