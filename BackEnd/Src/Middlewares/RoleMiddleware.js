bc0c56a61b81a982063181b6ac36ed40d7830516
Blamxis <gavinetm26@gmail.com>
2023-12-20 19:27:51 +0100

    Mise en place des permissions de roles

---
+++ BackEnd/Src/Middlewares/RoleMiddleware.js
@@ -0,0 +1,31 @@
+const { Role } = require('../Models/index');
+
+class RoleMiddleware {
+    static async isAdmin(req, res, next) {
+        try {
+            const userRole = await Role.findByPk(req.user.Id_role);
+            if (userRole && userRole.Nom === 'Administrateur') {
+                next();
+            } else {
+                res.status(403).send('Accès refusé. Requis: Rôle Administrateur');
+            }
+        } catch (error) {
+            res.status(500).send('Erreur Serveur');
+        }
+    }
+
+    static async isEmployee(req, res, next) {
+        try {
+            const userRole = await Role.findByPk(req.user.Id_role);
+            if (userRole && userRole.Nom === 'Employé') {
+                next();
+            } else {
+                res.status(403).send('Accès refusé. Requis: Rôle Employé');
+            }
+        } catch (error) {
+            res.status(500).send('Erreur Serveur');
+        }
+    }
+}
+
+module.exports = RoleMiddleware;
