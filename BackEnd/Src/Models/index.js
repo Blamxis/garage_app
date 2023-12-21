f9fc9a1bfc1595f7f97cfbeb85a410c31a8bed2f
Blamxis <gavinetm26@gmail.com>
2023-12-21 17:32:58 +0100

    Ajout de service dans le model central

--- BackEnd/Src/Models/index.js
+++ BackEnd/Src/Models/index.js
@@ -2,13 +2,18 @@ const db = require('../Utils/db.config');
 const sequelize = db.getSequelizeInstance();
 const User = require('./User')(sequelize);
 const Role = require('./Role')(sequelize);
+const Service = require('./Service')(sequelize);
 
 
 // Définition des associations
 User.belongsTo(Role, { foreignKey: 'Id_role' });
 Role.hasMany(User, { foreignKey: 'Id_role' });
 
+User.hasMany(Service, { foreignKey: 'Id_user' });
+Service.belongsTo(User, { foreignKey: 'Id_user' });
+
 module.exports = {
     User,
-    Role
+    Role,
+    Service
 };
