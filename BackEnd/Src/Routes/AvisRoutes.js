0baa72b6c3f4540e712bda03ed1b65a7f3b06843
Blamxis <gavinetm26@gmail.com>
2023-12-22 16:09:48 +0100

    renommage des routes

--- BackEnd/Src/Routes/AvisRoutes.js
+++ BackEnd/Src/Routes/AvisRoutes.js
@@ -12,16 +12,16 @@ class AvisRoutes {
     initRoutes() {
         this.router.post('/avis', AvisController.createAvis);
         // Routes disponible uniquement pour admin
-        this.router.get('/avis/admin', [ AuthMiddleware.authenticate, RoleMiddleware.isAdmin ], AvisController.getAllAvis);
-        this.router.get('/avis/admin/:id', [ AuthMiddleware.authenticate, RoleMiddleware.isAdmin ], AvisController.getAvisById);
-        this.router.put('/avis/admin/:id', [ AuthMiddleware.authenticate, RoleMiddleware.isAdmin ], AvisController.updateAvis);
-        this.router.delete('/avis/admin/:id', [ AuthMiddleware.authenticate, RoleMiddleware.isAdmin ], AvisController.deleteAvis);
+        this.router.get('/admin/avis', [ AuthMiddleware.authenticate, RoleMiddleware.isAdmin ], AvisController.getAllAvis);
+        this.router.get('/admin/avis/:id', [ AuthMiddleware.authenticate, RoleMiddleware.isAdmin ], AvisController.getAvisById);
+        this.router.put('/admin/avis/:id', [ AuthMiddleware.authenticate, RoleMiddleware.isAdmin ], AvisController.updateAvis);
+        this.router.delete('/admin/avis/:id', [ AuthMiddleware.authenticate, RoleMiddleware.isAdmin ], AvisController.deleteAvis);
 
         // Routes disponible pour employee
-        this.router.get('/avis/employee', [ AuthMiddleware.authenticate, RoleMiddleware.isEmployee ], AvisController.getAllAvis);
-        this.router.get('/avis/employee/:id', [ AuthMiddleware.authenticate, RoleMiddleware.isEmployee ], AvisController.getAvisById);
-        this.router.put('/avis/employee/:id', [ AuthMiddleware.authenticate, RoleMiddleware.isEmployee ], AvisController.updateAvis);
-        this.router.delete('/avis/employee/:id', [ AuthMiddleware.authenticate, RoleMiddleware.isEmployee ], AvisController.deleteAvis);
+        this.router.get('/employee/avis/', [ AuthMiddleware.authenticate, RoleMiddleware.isEmployee ], AvisController.getAllAvis);
+        this.router.get('/employee/avis/:id', [ AuthMiddleware.authenticate, RoleMiddleware.isEmployee ], AvisController.getAvisById);
+        this.router.put('/employee/avis/:id', [ AuthMiddleware.authenticate, RoleMiddleware.isEmployee ], AvisController.updateAvis);
+        this.router.delete('/employee/avis/:id', [ AuthMiddleware.authenticate, RoleMiddleware.isEmployee ], AvisController.deleteAvis);
     }
 
     getRouter() {
