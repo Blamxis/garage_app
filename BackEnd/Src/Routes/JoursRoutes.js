764a2f3a94ed322f0edbf9b713cab19a35033b8d
Blamxis <gavinetm26@gmail.com>
2023-12-22 16:07:35 +0100

    Ajout de JoursRoutes

---
+++ BackEnd/Src/Routes/JoursRoutes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const JoursController = require('../Controllers/JoursController');
+const AuthMiddleware = require('../Middlewares/AuthMiddleware');
+const RoleMiddleware = require('../Middlewares/RoleMiddleware');
+
+class JoursRoutes {
+    constructor() {
+        this.router = express.Router();
+        this.initRoutes();
+    }
+
+    initRoutes() {
+        this.router.post('/admin/jours', [AuthMiddleware.authenticate, RoleMiddleware.isAdmin], JoursController.createJour);
+        this.router.get('/admin/jours', [AuthMiddleware.authenticate, RoleMiddleware.isAdmin], JoursController.getAllJours);
+        this.router.get('/admin/jours/:id', [AuthMiddleware.authenticate, RoleMiddleware.isAdmin], JoursController.getJourById);
+        this.router.put('/admin/jours/:id', [AuthMiddleware.authenticate, RoleMiddleware.isAdmin], JoursController.updateJour);
+        this.router.delete('/admin/jours/:id', [AuthMiddleware.authenticate, RoleMiddleware.isAdmin], JoursController.deleteJour);
+    }
+
+    getRouter() {
+        return this.router;
+    }
+}
+
+module.exports = new JoursRoutes().getRouter();
