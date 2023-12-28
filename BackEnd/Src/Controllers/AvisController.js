98176b1eb6e2e3ebb0478944fcc8295dd6770cb6
Blamxis <gavinetm26@gmail.com>
2023-12-28 17:01:00 +0100

    Correction pb Date dans Update

--- BackEnd/Src/Controllers/AvisController.js
+++ BackEnd/Src/Controllers/AvisController.js
@@ -1,7 +1,6 @@
 const { Avis } = require("../Models/index");
 
 class AvisController {
-
   static convertirDateFrancaiseEnSQL(dateFrancaise) {
     const [jour, mois, annee] = dateFrancaise.split("/");
     return `${annee}-${mois}-${jour}`;
@@ -9,15 +8,15 @@ class AvisController {
 
   static async createAvis(req, res) {
     try {
-      const { Nom, Prenom, Description, Date, Note, Id_user } = req.body;
+      const { Nom, Prenom, Description, Date: dateReçue, Note, Id_user } = req.body;
 
       // Validation du format de la date (format français : jj/mm/aaaa)
       const regexDateFrancaise = /^\d{2}\/\d{2}\/\d{4}$/;
-      if (!regexDateFrancaise.test(Date)) {
+      if (!regexDateFrancaise.test(dateReçue)) {
         return res.status(400).json({ error: "Le format de la date doit être jj/mm/aaaa" });
       }
 
-      const dateSQL = AvisController.convertirDateFrancaiseEnSQL(Date);
+      const dateSQL = AvisController.convertirDateFrancaiseEnSQL(dateReçue);
 
       // Création de l'avis avec ou sans id_user
       const newAvis = await Avis.create({
@@ -41,7 +40,7 @@ class AvisController {
     try {
       const avis = await Avis.findAll();
       if (avis.length === 0) {
-        return res.status(200).json({ message: 'Aucun avis pour le moment' });
+        return res.status(200).json({ message: "Aucun avis pour le moment" });
       }
       res.status(200).json(avis);
     } catch (error) {
@@ -67,27 +66,41 @@ class AvisController {
   static async updateAvis(req, res) {
     const { id } = req.params;
     try {
-        console.log("Données reçues:", req.body);
-      const { Status } = req.body;
+      console.log("Données reçues:", req.body);
+      let { Date: dateReçue, Status } = req.body;
+
+      let dateTraitée;
+      if (dateReçue) {
+        const regexDateFrancaise = /^\d{2}\/\d{2}\/\d{4}$/;
+        if (!regexDateFrancaise.test(dateReçue)) {
+          return res.status(400).json({ error: "Le format de la date doit être jj/mm/aaaa" });
+        }
+        dateTraitée = AvisController.convertirDateFrancaiseEnSQL(dateReçue);
+      } else {
+        dateTraitée = new Date().toISOString().slice(0, 10);
+      }
 
       // Validation du statut
       if (Status && !["en attente", "approuvé", "rejeté"].includes(Status)) {
         return res.status(400).json({ error: "Statut invalide" });
       }
-
+      
       // Mise à jour de l'avis
-      const updateData = req.body;
+      const updateData = { Date: dateTraitée, Status };
       const [updated] = await Avis.update(updateData, {
         where: { Id_avis: id },
       });
 
-      if (updated) {
+      if (updated === 0) {
+        // Si aucun avis modifié
+        res.status(404).json({ message: "Avis non trouvé" });
+      } else {
+        // Si avis mis a jour avec succès
         const updatedAvis = await Avis.findByPk(id);
         res.status(200).json(updatedAvis);
-      } else {
-        res.status(404).json({ message: "Avis non trouvé" });
       }
     } catch (error) {
+      console.error(error);
       res.status(500).json({ error: "Erreur serveur" });
     }
   }
