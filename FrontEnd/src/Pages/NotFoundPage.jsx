569f24cf6929a99ea194322ec03f67ac90546429
Blamxis <gavinetm26@gmail.com>
2024-01-20 15:54:08 +0100

    Ajout Page NotFound

---
+++ FrontEnd/src/Pages/NotFoundPage.jsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+    return (
+      <div>
+        {/* Contenu de la page Mécanique */}
+        <h1>Page de NotFound error 404</h1>
+        {/* Ajoutez le contenu supplémentaire de votre page ici */}
+      </div>
+    );
+  };
+  
+  export default NotFound;
