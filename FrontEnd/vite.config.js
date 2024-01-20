ce3d65a71eb80986a3d01c4811a12b4a00ec9ff3
Blamxis <gavinetm26@gmail.com>
2024-01-20 12:24:19 +0100

    Config Proxy

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://192.168.1.95:8888',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
+    }
+  }
+});
