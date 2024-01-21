30e9afbba97b485e5ff99770845557480ee9b7e9
Blamxis <gavinetm26@gmail.com>
2024-01-21 13:33:00 +0100

    Creation Composant Footer

---
+++ FrontEnd/src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+      <footer className="footer">
+        <div className="container">
+          <div className="footer-content">
+            <div className="left">Groupe Parrot &copy; 2024</div>
+            <div className="right">Mentions Légales</div>
+          </div>
+        </div>
+      </footer>
+    );
+  };
+  
+  export default Footer;
