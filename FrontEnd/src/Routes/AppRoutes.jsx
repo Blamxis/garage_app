008f517b2bff201d28c0a446422cb71321f93f65
Blamxis <gavinetm26@gmail.com>
2024-01-21 23:37:02 +0100

    Mise en place de la protection des routes amdin et employé

--- FrontEnd/src/Routes/AppRoutes.jsx
+++ FrontEnd/src/Routes/AppRoutes.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useAuth } from '../Context/AuthContext'; 
 
 import HomePage from '../Pages/HomePage';
 import Mecanique from '../Pages/Mecanique';
@@ -14,22 +15,32 @@ import DashboardEmployee from '../Pages/DashboardEmployee';
 import NotFoundPage from '../Pages/NotFoundPage';
 
 const AppRoutes = () => {
-    return (
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/mecanique" element={<Mecanique />} />
-        <Route path="/carrosserie" element={<Carrosserie />} />
-        <Route path="/revision" element={<Revision />} />
-        <Route path="/parc-auto" element={<ParcAuto />} />
-        <Route path="/parc-auto/:id" element={<CarInfos />} />
-        <Route path="/avis" element={<Avis />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/connexion" element={<Connexion />} />
-        <Route path="/admin/dashboard" element={<DashboardAdmin />} />
-        <Route path="/employee/dashboard" element={<DashboardEmployee />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    );
-  };
-  
-  export default AppRoutes;
+  const { isAuthenticated } = useAuth();
+
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/mecanique" element={<Mecanique />} />
+      <Route path="/carrosserie" element={<Carrosserie />} />
+      <Route path="/revision" element={<Revision />} />
+      <Route path="/parc-auto" element={<ParcAuto />} />
+      <Route path="/parc-auto/:id" element={<CarInfos />} />
+      <Route path="/avis" element={<Avis />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/connexion" element={<Connexion />} />
+      <Route 
+        path="/admin/dashboard" 
+        element={isAuthenticated ? <DashboardAdmin /> : <Navigate to="/connexion" />}
+      />
+      <Route 
+        path="/employee/dashboard" 
+        element={isAuthenticated ? <DashboardEmployee /> : <Navigate to="/connexion" />}
+      />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
+
+
