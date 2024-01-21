2cf7c95d24f30d45f8580a8022438329f528b32a
Blamxis <gavinetm26@gmail.com>
2024-01-21 13:34:29 +0100

    modif

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
+      '/api/': {
         target: 'http://192.168.1.95:8888',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
