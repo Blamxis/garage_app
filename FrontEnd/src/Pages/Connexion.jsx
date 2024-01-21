eb57afedf2d2a4cfb0e75ad24974305908134fb8
Blamxis <gavinetm26@gmail.com>
2024-01-21 23:35:15 +0100

    Ajout de la page Connexion

--- FrontEnd/src/Pages/Connexion.jsx
+++ FrontEnd/src/Pages/Connexion.jsx
@@ -1,13 +1,31 @@
 import CustomNavbar from "../Components/Navbar";
+import LoginForm from '../Components/LoginForm';
+import logoImage from "../assets/Images/Logo_FInal-ok.png";
+import Footer from "../Components/Footer";
+
 
 const Connexion = () => {
     return (
-      <div>
+      <div className="connexion-container">
         <CustomNavbar />
-        <h1>Page de Connexion</h1>
-        {/* Ajoutez le contenu supplémentaire de votre page ici */}
+        <div className="login-area">
+          <LoginForm />
+        </div>
+        <div className="contact-info">
+          <p>Contactez-nous</p>
+          <p>01 02 03 04 05</p>
+        </div>
+        <div className="social-media-links">
+          {/* Ici, utilisez les icônes appropriées ou des images */}
+        </div>
+        <div className="logo">
+          <img src={logoImage} alt="Logo" className="custom-logo" />
+        </div>
+        <div className="footer">
+          <Footer />
+        </div>
       </div>
     );
-  };
-  
-  export default Connexion;
+};
+
+export default Connexion;
