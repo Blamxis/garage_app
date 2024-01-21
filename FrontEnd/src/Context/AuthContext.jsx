2af46d170484f7796e23bd907204df95376d3e1b
Blamxis <gavinetm26@gmail.com>
2024-01-21 23:35:41 +0100

    Ajout d'une centralisation d'authentification

---
+++ FrontEnd/src/Context/AuthContext.jsx
@@ -0,0 +1,31 @@
+import { createContext , useContext , useState } from 'react';
+import PropTypes from 'prop-types';
+
+export const AuthContext = createContext(null);
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const login = (token) => {
+    localStorage.setItem('authToken', token);
+    setIsAuthenticated(true);
+  };
+
+  const logout = () => {
+    localStorage.removeItem('authToken');
+    setIsAuthenticated(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+// Ajout de la validation PropTypes pour children
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
