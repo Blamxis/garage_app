7e3a9c0cea566662e49f4a6ee2a6141c004e09c1
Blamxis <gavinetm26@gmail.com>
2024-01-24 14:29:10 +0100

    Ajout dossier Testimonial

---
+++ FrontEnd/src/Components/Testimonials/Testimonial.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+
+const Testimonial = ({ name, prenom, description, date, rating }) => {
+  const renderStars = () => {
+    return [...Array(rating)].map((_, i) => <span key={i}>⭐</span>);
+  };
+
+  return (
+    <div className="testimonial">
+      <div className="testimonial-header">
+        <h3>{name} {prenom}</h3>
+        <div>{renderStars()}</div>
+      </div>
+      <p className="testimonial-description">{description}</p>
+      <p className="testimonial-date">{date}</p>
+    </div>
+  );
+};
+
+Testimonial.propTypes = {
+  name: PropTypes.string.isRequired,
+  prenom: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
+
+export default Testimonial;
