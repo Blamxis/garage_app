2dcb1ebe2320f7ad10d2adda9dd4f936ddcfc4a2
Blamxis <gavinetm26@gmail.com>
2024-01-24 14:23:33 +0100

    Ajout dossier avec component

---
+++ FrontEnd/src/Components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import './Footer.scss';
+
+const Footer = () => {
+    return (
+      <footer className="footer">
+        <div className="container">
+          <div className="footer-content">
+            <div className="left">Groupe Parrot &copy; 2024</div>
+            <div className="right">Mentions Légales</div>
+          </div>
+        </div>
+      </footer>
+    );
+  };
+  
+  export default Footer;
