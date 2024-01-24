ac3f3d7aba819bc8d15a2fe7f9b16d7623f7a651
Blamxis <gavinetm26@gmail.com>
2024-01-24 14:21:42 +0100

    Ajout route imbrique dashboard

--- FrontEnd/src/Routes/AppRoutes.jsx
+++ FrontEnd/src/Routes/AppRoutes.jsx
@@ -14,6 +14,19 @@ import DashboardAdmin from '../Pages/DashboardAdmin';
 import DashboardEmployee from '../Pages/DashboardEmployee';
 import NotFoundPage from '../Pages/NotFoundPage';
 
+// Routes Dashboard
+
+import Users from '../Pages/UserDash';
+import Annonces from '../Pages/AnnonceDash';
+import Services from '../Pages/ServicesDash';
+import Voitures from '../Pages/VoituresDash';
+import Marques from '../Pages/MarquesDash';
+import Modeles from '../Pages/ModeleDash';
+import OptionsEquipements from '../Pages/OptionsDash';
+import Messages from '../Pages/MessagesDash';
+import AvisClients from '../Pages/AvisDash';
+import HorairesGarage from '../Pages/HorairesDash';
+
 const AppRoutes = () => {
   const { isAuthenticated } = useAuth();
 
@@ -28,14 +41,31 @@ const AppRoutes = () => {
       <Route path="/avis" element={<Avis />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/connexion" element={<Connexion />} />
-      <Route 
-        path="/admin/dashboard" 
-        element={isAuthenticated ? <DashboardAdmin /> : <Navigate to="/connexion" />}
-      />
-      <Route 
-        path="/employee/dashboard" 
-        element={isAuthenticated ? <DashboardEmployee /> : <Navigate to="/connexion" />}
-      />
+      <Route path="/admin/dashboard" element={isAuthenticated ? <DashboardAdmin /> : <Navigate to="/connexion" />}>
+      <Route index element={<Navigate to="users" />} />
+        <Route path="users" element={<Users />} />
+        <Route path="annonces" element={<Annonces />} />
+        <Route path="services" element={<Services />} />
+        <Route path="voitures" element={<Voitures />} />
+        <Route path="marques" element={<Marques />} />
+        <Route path="modeles" element={<Modeles />} />
+        <Route path="options" element={<OptionsEquipements />} />
+        <Route path="messages" element={<Messages />} />
+        <Route path="avis-clients" element={<AvisClients />} />
+        <Route path="horaires-garage" element={<HorairesGarage />} />
+      </Route>
+      <Route path="/employee/dashboard" element={isAuthenticated ? <DashboardEmployee /> : <Navigate to="/connexion" />}>
+      <Route index element={<Navigate to="annonces" />} />
+        <Route path="annonces" element={<Annonces />} />
+        <Route path="services" element={<Services />} />
+        <Route path="voitures" element={<Voitures />} />
+        <Route path="marques" element={<Marques />} />
+        <Route path="modeles" element={<Modeles />} />
+        <Route path="options" element={<OptionsEquipements />} />
+        <Route path="messages" element={<Messages />} />
+        <Route path="avis-clients" element={<AvisClients />} />
+        <Route path="horaires-garage" element={<HorairesGarage />} />
+      </Route>
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
