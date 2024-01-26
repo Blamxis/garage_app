c91b68de233aacf8d444346f03676b03cbf18a55
Blamxis <gavinetm26@gmail.com>
2024-01-26 18:05:18 +0100

    Modif config

--- FrontEnd/src/Api/AxiosConfig.js
+++ FrontEnd/src/Api/AxiosConfig.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.REACT_APP_API_URL, // URL de base d'API
-  timeout: 10000, // délai d'attente pour les requêtes
+  baseURL: import.meta.env.VITE_API_URL,
+  timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
   },
