1dfede2e424fc790d23921bf5649144fed34e6df
Blamxis <gavinetm26@gmail.com>
2024-01-26 18:16:04 +0100

    Modifications

--- FrontEnd/src/Context/AuthContext.jsx
+++ FrontEnd/src/Context/AuthContext.jsx
@@ -1,5 +1,14 @@
-import { createContext , useContext , useState } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+
+function jwtDecode(token) {
+  try {
+    return JSON.parse(atob(token.split(".")[1]));
+  } catch (error) {
+    console.error("Erreur lors du décodage du token :", error);
+    return null;
+  }
+}
 
 export const AuthContext = createContext(null);
 
@@ -7,25 +16,39 @@ export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [decodedToken, setDecodedToken] = useState(null);
+
+  useEffect(() => {
+
+    const token = localStorage.getItem('authToken');
+
+    if (token) {
+      const decoded = jwtDecode(token);
+      setDecodedToken(decoded);
+      setIsAuthenticated(true);
+    }
+  }, []);
 
   const login = (token) => {
     localStorage.setItem('authToken', token);
+    const decoded = jwtDecode(token);
+    setDecodedToken(decoded);
     setIsAuthenticated(true);
   };
 
   const logout = () => {
     localStorage.removeItem('authToken');
+    setDecodedToken(null);
     setIsAuthenticated(false);
   };
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+    <AuthContext.Provider value={{ isAuthenticated, decodedToken, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
 };
 
-// Ajout de la validation PropTypes pour children
 AuthProvider.propTypes = {
   children: PropTypes.node.isRequired,
 };
