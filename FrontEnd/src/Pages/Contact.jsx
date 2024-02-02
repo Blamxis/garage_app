b4f0eab630069db0a739a47af12321ce3f69a3ef
Blamxis <gavinetm26@gmail.com>
2024-02-03 00:39:52 +0100

    Ajout formulaire a la page

--- FrontEnd/src/Pages/Contact.jsx
+++ FrontEnd/src/Pages/Contact.jsx
@@ -1,12 +1,24 @@
+import Footer from "../Components/Footer/Footer";
+import FormContact from "../Components/FormContact/FormContact";
 import CustomNavbar from "../Components/NavBar/Navbar";
+import logoImage from '../assets/Images/Logo_FInal-ok.png'; 
 
 const Contact = () => {
     return (
-      <div>
+      <div className="contact-container">
         <CustomNavbar />
-        <h1>Page de Contact</h1>
+        <div className="contact-header">
+          <h1>- CONTACT -</h1>
+        </div>
+        <div className="content">
+          <div className="form-contact">
+            <FormContact />
+          </div>
+          <img src={logoImage} alt="Logo" className="contact-logo" />
+        </div>
+        <Footer />
       </div>
     );
-  };
-  
-  export default Contact;
+};
+
+export default Contact;
