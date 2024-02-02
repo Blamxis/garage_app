d7cd2a96326675316d77b362581065ea493a54e7
Blamxis <gavinetm26@gmail.com>
2024-02-03 00:38:38 +0100

    Page Carroserie Terminé

--- FrontEnd/src/Pages/Carrosserie.jsx
+++ FrontEnd/src/Pages/Carrosserie.jsx
@@ -1,10 +1,18 @@
 import CustomNavbar from "../Components/NavBar/Navbar";
+import FormServices from "../Components/FormServices/FormServices";
+import Footer from "../Components/Footer/Footer";
+import CarrossImage from '../assets/Images/carrosserie.jpg';
 
 const Carrosserie = () => {
   return (
-    <div>
+    <div className="service-container">
       <CustomNavbar />
-      <h1>Page de Carrosserie</h1>
+      <h1>- CARROSSERIE D&apos;EXCELLENCE -</h1>
+      <div className="content">
+        <FormServices serviceType="Carrosserie" />
+        <img src={CarrossImage} alt="carrosserie" className="sidebar-image"/>
+      </div>
+      <Footer />
     </div>
   );
 };
