22fbef26d092e64d8810e8f7a7dca99cea820922
Blamxis <gavinetm26@gmail.com>
2024-02-03 00:39:18 +0100

    Page Révision Terminé

--- FrontEnd/src/Pages/Revision.jsx
+++ FrontEnd/src/Pages/Revision.jsx
@@ -1,12 +1,20 @@
 import CustomNavbar from "../Components/NavBar/Navbar";
+import FormServices from "../Components/FormServices/FormServices";
+import Footer from "../Components/Footer/Footer";
+import RevisImage from '../assets/Images/revision.jpg';
 
 const Revision = () => {
-    return (
-      <div>
-        <CustomNavbar />
-        <h1>Page de Révision</h1>
+  return (
+    <div className="service-container">
+      <CustomNavbar />
+      <h1>- RÉVISION COMPLÈTE -</h1>
+      <div className="content">
+        <FormServices serviceType="Révision" />
+        <img src={RevisImage} alt="revision" className="sidebar-image"/>
       </div>
-    );
-  };
-  
-  export default Revision;
+      <Footer />
+    </div>
+  );
+};
+
+export default Revision;
