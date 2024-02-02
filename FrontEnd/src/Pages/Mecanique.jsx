40c41a42bf91b45fac9c6871c6591b1f06b9474e
Blamxis <gavinetm26@gmail.com>
2024-02-03 00:38:58 +0100

    Page Mecanique Terminé

--- FrontEnd/src/Pages/Mecanique.jsx
+++ FrontEnd/src/Pages/Mecanique.jsx
@@ -1,12 +1,21 @@
 import CustomNavbar from "../Components/NavBar/Navbar";
+import FormServices from "../Components/FormServices/FormServices";
+import Footer from "../Components/Footer/Footer";
+import mecanoImage from '../assets/Images/mecano.jpg';
 
 const Mecanique = () => {
   return (
-    <div>
+    <div className="service-container">
       <CustomNavbar />
-      <h1>Page de Mécanique</h1>
+      <h1>- MÉCANIQUE DE PRÉCISION -</h1>
+      <div className="content">
+        <FormServices serviceType="Mécanique" />
+        <img src={mecanoImage} alt="mecano" className="sidebar-image"/>
+      </div>
+      <Footer />
     </div>
   );
 };
 
 export default Mecanique;
+
