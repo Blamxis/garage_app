35035e33b4f11ad4e8efe578c458ff9122ff9b79
Blamxis <gavinetm26@gmail.com>
2024-02-14 13:58:57 +0100

    Modification route pour que user puisse soumettre form sans connexion

--- BackEnd/Src/Routes/MessageRoutes.js
+++ BackEnd/Src/Routes/MessageRoutes.js
@@ -10,12 +10,9 @@ class MessageRoutes {
     }
 
     initRoutes() {
-        // La route pour créer un message reste ouverte sans authentification spécifique
-        // en supposant que tout utilisateur authentifié peut envoyer un message
-        this.router.post('/messages', AuthMiddleware.authenticate, MessageController.createMessage);
+        
+        this.router.post('/messages', MessageController.createMessage);
 
-        // Routes pour gérer les messages, disponibles à la fois pour l'admin et l'employé
-        // Utilisation d'une méthode unique isEmployeeOrAdmin (à définir si elle n'existe pas déjà) pour simplifier
         this.router.get('/messages', [AuthMiddleware.authenticate, RoleMiddleware.isEmployeeOrAdmin], MessageController.getAllMessages);
         this.router.get('/messages/:id', [AuthMiddleware.authenticate, RoleMiddleware.isEmployeeOrAdmin], MessageController.getMessageById);
         this.router.put('/messages/:id', [AuthMiddleware.authenticate, RoleMiddleware.isEmployeeOrAdmin], MessageController.updateMessage);
@@ -26,6 +23,7 @@ class MessageRoutes {
         return this.router;
     }
 }
+
 
 module.exports = new MessageRoutes().getRouter();
 
