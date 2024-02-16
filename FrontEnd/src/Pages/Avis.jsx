2a7819317d6b5ca94d75eea4c3ccf3fd130ba114
Blamxis <gavinetm26@gmail.com>
2024-02-16 09:40:40 +0100

    Finalisation de la page Avis

--- FrontEnd/src/Pages/Avis.jsx
+++ FrontEnd/src/Pages/Avis.jsx
@@ -1,12 +1,18 @@
 import CustomNavbar from "../Components/NavBar/Navbar";
+import StarRatingForm from '../Components/AvisForm/AvisForm';
+import Footer from '../Components/Footer/Footer';
 
 const Avis = () => {
     return (
-      <div>
-        <CustomNavbar />
-        <h1>Page de Avis</h1>
-      </div>
+        <div className="page-avis">
+            <CustomNavbar />
+            <h1>- LAISSER NOUS UN AVIS -</h1>
+            <StarRatingForm />
+            <div className="footer-container">
+                <Footer />
+            </div>
+        </div>
     );
-  };
-  
-  export default Avis;
+};
+
+export default Avis;
