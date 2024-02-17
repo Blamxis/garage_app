d3c755ba9be3b0817bcaf9d006686e20f9fb32c1
Blamxis <gavinetm26@gmail.com>
2024-02-17 23:39:10 +0100

    mise a jour proxy

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api/': {
-        target: 'http://192.168.1.95:8888',
+        target: 'http://blamxis.alwaysdata.net',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       },
