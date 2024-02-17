b7c9f9ce5d3d70aaf3da0b01d6c0c09f6eaa757f
Blamxis <gavinetm26@gmail.com>
2024-02-17 17:17:27 +0100

    Integration de react-helmet et encapsulage de l'app pour les metas

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.jsx'
 import './scss/App.scss';
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>,
 )
