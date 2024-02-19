506eb43326cdd18ade3a93409af72b4842c25eea
Blamxis <gavinetm26@gmail.com>
2024-02-19 17:52:47 +0100

    Modif cors

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -39,7 +39,7 @@ class Server {
     configMiddlewares() {
         
         this.app.use(cors({
-            origin: 'http://localhost:5173',
+            origin: '*',
             methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
             credentials: true,
         })); // Activation de CORS
